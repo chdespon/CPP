a9401bdbbb1551eba0d26d6c226b5c94282f6d9d
Charles DESPONT <chdespon@e1r8p21.42.fr>
2022-04-27 18:17:44 +0200

    remove error fonction

--- day_01/ex04/includes/Replace.hpp
+++ day_01/ex04/includes/Replace.hpp
@@ -6,7 +6,7 @@
 /*   By: chdespon <chdespon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 13:13:41 by chdespon          #+#    #+#             */
-/*   Updated: 2022/04/27 16:05:41 by chdespon         ###   ########.fr       */
+/*   Updated: 2022/04/27 18:16:27 by chdespon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ class Replace
 		std::string	getReplace(void) const {return _replace;}
 		bool		getError(void) const {return _error;}
 
-		bool	check_args(int argc);
+		void	replace_line(std::ifstream &file);
 };
 
 #endif
